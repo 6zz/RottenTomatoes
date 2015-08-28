2a9a5d49fff8076371eb731802e0a1ac83a89e90
Shawn Zhu <shawn@tumblr.com>
2015-08-28 00:23:21 -0700

    add movies request

--- Rotten Tomatoes/ListingViewController.swift
+++ Rotten Tomatoes/ListingViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class ListingViewController: UIViewController {
-
+    var movies: NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
+
+            self.movies = responseDictionary["movies"] as? NSArray
+//            self.tableView.reloadData()
+            
+            NSLog("response: \(self.movies)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
